e6349f9f12be3a4a47a40a379c5ee4a1c6b451f4
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-15 14:02:15 -0800

    Add routes to components in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import {AllKegsEmployee} from './employee/AllKegsEmployee';
+import {AllKegsPatron} from './patron/AllKegsPatron';
+import {NewKegControl} from './employee/NewKegControl';
+import {EditKegControl} from './employee/EditKegControl';
 
 function App() {
   return (
     <div>
-
-      {/* <Switch>
-          <Route exact path='/' component={TicketList} />
-          <Route path='/newticket' component={NewTicketForm}/>
-        </Switch> */}
+      <Switch>
+        <Route exact path='/' component={AllKegsPatron} />
+        <Route exact path='/employee' component={AllKegsEmployee} />
+        <Route path='/keg/new' component={NewKegControl}/>
+        <Route path='/keg/edit/{id}' component={EditKegControl} />
+      </Switch>
     </div>
   );
 }
