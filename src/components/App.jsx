610b9a8f16a82bf0af4d88d62ff1c2d48afba9b4
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-15 08:35:41 -0800

    Add initial react view files and src folder structure

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+
+      {/* <Switch>
+          <Route exact path='/' component={TicketList} />
+          <Route path='/newticket' component={NewTicketForm}/>
+        </Switch> */}
+    </div>
+  );
+}
+
+export default App;
