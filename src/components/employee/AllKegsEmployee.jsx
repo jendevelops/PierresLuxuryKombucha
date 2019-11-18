bec5ab8aabd422bd69d7a4b9e9fc167251f49419
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-17 20:34:59 -0800

    Fix employee componenet with encapsulating {} and omitting {} for import statment on default export

--- src/components/employee/AllKegsEmployee.jsx
+++ src/components/employee/AllKegsEmployee.jsx
@@ -1,59 +1,21 @@
-import {React} from 'react';
-import {EmployeeKeg} from './EmployeeKeg';
+import React from 'react';
+import EmployeeKeg from './EmployeeKeg';
+import keggerinos from './../Keggerinos';
 
-let keggerinos = [
-  {
-    name: 'Peach Fuzzies',
-    flavor: 'Fruit',
-    price: 4.75,
-    pintsLeft: 107,
-    description: 'Imgine a lovely peach in the summertime. Jucy and supple, all bottled for your kombucha drinking pleasure.'
-  },
-  {
-    name: 'Blues Berry Blast',
-    flavor: 'Fruit',
-    price: 4.50,
-    pintsLeft: 43,
-    description: 'Only the bluest of blue bois goes in this one of a kind blue berry and cayenne pepper kombucha. A sweet lull before you\'re BLASTED off into upper stratasphere of taste.'
-  },
-  {
-    name: 'Belgian Delight',
-    flavor: 'Chocolate',
-    price: 6.00,
-    pintsLeft: 65,
-    description: 'We have partnered with the finest belgian chocoletiers to give you the decadant, roasted flavor of the Belgian Delight. The deep notes of the toasted cocoa and belgian chocolate nibs create a truely unique kombucha experience.'
-  },
-  {
-    name: 'The OG',
-    flavor: 'Vegetable',
-    price: 3.00,
-    pintsLeft: 1,
-    description: 'The one that started it all. As a play on words for the Japanese meaning of kombu meaning seaweed and cha meaning tea, we introduced the world to the luxury that is Pierre\'s Luxury Kombucha with a seaweed flavored kombucha. Wild konbu harvested off the shores of Japan roasted to perfection create this wondefully rich and complex kombucha.'
-  },
-  {
-    name: 'Bagels Reimagined',
-    flavor: 'Grain',
-    price: 10.00,
-    pintsLeft: 15,
-    description: 'In partnership with Pierre\'s close friend Jaqueline of the world renowned pop up shop Bagels Reimagined, this kombucha drink is where have none have gone before. With dairy hints and a heavy grain base, this kombucha will leave your mouth tasting like essence of the reimagined butter bagel with whole fish.'
-  }
-];
+
 // props needed: name, flavor, price, pintsLeft, key
 function AllKegsEmployee(){
   return(
-    <div id="AllKegsEmployee">
-      keggerinos.map((keg, index) =>
-      <EmployeeKeg 
-        name={keg.name}
-        flavor = {keg.flavor}
-        price = {keg.price}
-        pintsLeft = {kegs.pintsLeft}
-        key={index} />
-      );
+    <div>
+      {keggerinos.map((keg, index) =>
+        <EmployeeKeg 
+          name={keg.name}
+          flavor = {keg.flavor}
+          price = {keg.price}
+          pintsLeft = {keg.pintsLeft}
+          key={index} />
+      )};
     </div>
   );
 }
-
-
-
 export default AllKegsEmployee;
--- src/components/employee/EmployeeKeg.jsx
+++ src/components/employee/EmployeeKeg.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 function EmployeeKeg(props){
   return(
     <div>
-      <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">{props.name}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">{props.flavor} | {props.price}</h6>
-          <p class="card-text">{props.pintsLeft}</p>
-          <p class="card-text">{props.description}</p>
-          <a href="/keg/sell/{props.key}" class="btn btn-warning">Sell Pint</a>
-          <a href="/keg/edit/{props.key}" class="btn btn-info">Edit Keg</a>
+      <div className="card col-4 inline">
+        <div className="card-body">
+          <h5 className="card-title">{props.name}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{props.flavor} | {props.price}</h6>
+          <p className="card-text">{props.pintsLeft}</p>
+          <p className="card-text">{props.description}</p>
+          <a href="/keg/sell/{props.key}" className="btn btn-warning">Sell Pint</a>
+          <a href="/keg/edit/{props.key}" className="btn btn-info">Edit Keg</a>
         </div>
       </div>
     </div>
