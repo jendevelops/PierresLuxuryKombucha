f98eb4ee817189e29e7289c6f09e8687ba5c1736
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-20 00:44:09 -0800

    Edit splash page to have fancy cursive font for logo

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -5,7 +5,7 @@ function splashPage(){
   return(
     <div className='wrapper'>
       <div className='splash'>
-        <h1 className='logo'><em>Pierre's</em> LuxKombucha</h1>
+        <h1 className='logo'>Luxury Kombucha</h1>
         <blockquote className='blockquote'>
           <p className="mb-0">The <em>finest</em> ingredients. The <em>finest</em> Kombucha.</p>
           <footer className="blockquote-footer">Pierre</footer>
