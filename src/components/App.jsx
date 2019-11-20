4c95952e5029981fdd4291da2641ff64e4684839
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-20 00:43:18 -0800

    Add header back into app and edit routes, delete logo to header and add new links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,18 @@ import {NewKegControl} from './employee/NewKegControl';
 import {EditKegControl} from './employee/EditKegControl';
 import Header from './Header';
 import splashPage from './SplashPage';
+import EmployeeLogin from './employee/EmployeeLogin';
 
 function App() {
   return (
     <div>
+      <Header />
+      
       <Switch>
         <Route exact path='/' component={splashPage} />
-        <Route exact path='/patron' component={AllKegsPatron} />
+        <Route exact path='/menu' component={AllKegsPatron} />
         <Route exact path='/employee' component={AllKegsEmployee} />
+        <Route exact path='/employee/login' component={EmployeeLogin} />
         <Route path='/keg/new' component={NewKegControl}/>
         <Route path='/keg/edit/{id}' component={EditKegControl} />
       </Switch>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-      <a className="navbar-brand" href="#">Pierre's Luxury Kombucha</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -11,21 +10,13 @@ function Header() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#/patron">Patron View <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="#/">Home<span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#/menu">Menu<span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/employee">EmployeeView</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Filter By
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#/patron/type">Type</a>
-              <a className="dropdown-item" href="#/patron/price">Price</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">Something else here</a>
-            </div>
+            <a className="nav-link" href="#/employee/login">Login</a>
           </li>
         </ul>
       </div>
