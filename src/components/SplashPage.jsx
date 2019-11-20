cdd315e285cb1440f80d60ef520e8f9d7dc9378d
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-19 23:14:08 -0800

    Add splash page and styling

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import '../css/SplashPage.css';
 
 function splashPage(){
-  return(<div></div>);
+  return(
+    <div className='wrapper'>
+      <div className='splash'>
+        <h1 className='logo'><em>Pierre's</em> LuxKombucha</h1>
+        <blockquote className='blockquote'>
+          <p className="mb-0">The <em>finest</em> ingredients. The <em>finest</em> Kombucha.</p>
+          <footer className="blockquote-footer">Pierre</footer>
+        </blockquote>
+      </div>
+    </div>
+  );
 }
 
 export default splashPage;
