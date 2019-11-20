3d8cb88bf077504932e293465768af49a1dd1116
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-20 09:30:12 -0800

    Change route to edit form in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import AllKegsEmployee from './employee/AllKegsEmployee';
 import AllKegsPatron from './patron/AllKegsPatron';
 import {NewKegControl} from './employee/NewKegControl';
-import {EditKegControl} from './employee/EditKegControl';
+import EditKegControl from './employee/EditKegControl';
 import Header from './Header';
 import splashPage from './SplashPage';
 import EmployeeLogin from './employee/EmployeeLogin';
@@ -12,14 +12,13 @@ function App() {
   return (
     <div>
       <Header />
-      
       <Switch>
         <Route exact path='/' component={splashPage} />
         <Route exact path='/menu' component={AllKegsPatron} />
         <Route exact path='/employee' component={AllKegsEmployee} />
         <Route exact path='/employee/login' component={EmployeeLogin} />
         <Route path='/keg/new' component={NewKegControl}/>
-        <Route path='/keg/edit/{id}' component={EditKegControl} />
+        <Route path='/keg/edit/' component={EditKegControl} />
       </Switch>
     </div>
   );
