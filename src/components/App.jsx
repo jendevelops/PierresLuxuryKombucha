e94fe6a791c75c16e87449d76c5c642d37c21bef
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-19 22:19:55 -0800

    Change routes to include splash page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,15 @@ import AllKegsPatron from './patron/AllKegsPatron';
 import {NewKegControl} from './employee/NewKegControl';
 import {EditKegControl} from './employee/EditKegControl';
 import Header from './Header';
+import splashPage from './SplashPage';
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path='/' component={AllKegsPatron} />
+        <Route exact path='/' component={splashPage} />
+        <Route exact path='/patron' component={AllKegsPatron} />
         <Route exact path='/employee' component={AllKegsEmployee} />
         <Route path='/keg/new' component={NewKegControl}/>
         <Route path='/keg/edit/{id}' component={EditKegControl} />
