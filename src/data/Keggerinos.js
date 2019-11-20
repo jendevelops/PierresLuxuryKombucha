96dc4da4c3670af9c3a71c25853cd6a0d5575564
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-20 09:31:26 -0800

    Add file urls to keg data

--- src/data/Keggerinos.js
+++ src/data/Keggerinos.js
@@ -3,35 +3,40 @@ const keggerinos = [{
   flavor: 'Fruit',
   price: 4.75,
   pintsLeft: 107,
-  description: 'Imgine a lovely peach in the summertime. Jucy and supple, all bottled for your kombucha drinking pleasure.'
+  description: 'Imgine a lovely peach in the summertime. Jucy and supple, all bottled for your kombucha drinking pleasure.',
+  img: '../img/peach.jpg'
 },
 {
   name: 'Blues Berry Blast',
   flavor: 'Fruit',
   price: 4.50,
   pintsLeft: 43,
-  description: 'Only the bluest of blue bois goes in this one of a kind blue berry and cayenne pepper kombucha. A sweet lull before you\'re BLASTED off into upper stratasphere of taste.'
+  description: 'Only the bluest of blue bois goes in this one of a kind blue berry and cayenne pepper kombucha. A sweet lull before you\'re BLASTED off into upper stratasphere of taste.',
+  img: 'blueberry.jpg'
 },
 {
   name: 'Belgian Delight',
   flavor: 'Chocolate',
   price: 6.00,
   pintsLeft: 65,
-  description: 'We have partnered with the finest belgian chocoletiers to give you the decadant, roasted flavor of the Belgian Delight. The deep notes of the toasted cocoa and belgian chocolate nibs create a truely unique kombucha experience.'
+  description: 'We have partnered with the finest belgian chocoletiers to give you the decadant, roasted flavor of the Belgian Delight. The deep notes of the toasted cocoa and belgian chocolate nibs create a truely unique kombucha experience.',
+  img: 'chocolate.jpg'
 },
 {
   name: 'The OG',
   flavor: 'Vegetable',
   price: 3.00,
   pintsLeft: 1,
-  description: 'The one that started it all. As a play on words for the Japanese meaning of kombu meaning seaweed and cha meaning tea, we introduced the world to the luxury that is Pierre\'s Luxury Kombucha with a seaweed flavored kombucha. Wild konbu harvested off the shores of Japan roasted to perfection create this wondefully rich and complex kombucha.'
+  description: 'The one that started it all. As a play on words for the Japanese meaning of kombu meaning seaweed and cha meaning tea, we introduced the world to the luxury that is Pierre\'s Luxury Kombucha with a seaweed flavored kombucha. Wild konbu harvested off the shores of Japan roasted to perfection create this wondefully rich and complex kombucha.',
+  img: 'konbu.jpg'
 },
 {
   name: 'Bagels Reimagined',
   flavor: 'Grain',
   price: 10.00,
   pintsLeft: 15,
-  description: 'In partnership with Pierre\'s close friend Jaqueline of the world renowned pop up shop Bagels Reimagined, this kombucha drink is where have none have gone before. With dairy hints and a heavy grain base, this kombucha will leave your mouth tasting like essence of the reimagined butter bagel with whole fish.'
+  description: 'In partnership with Pierre\'s close friend Jaqueline of the world renowned pop up shop Bagels Reimagined, this kombucha drink is where have none have gone before. With dairy hints and a heavy grain base, this kombucha will leave your mouth tasting like essence of the reimagined butter bagel with whole fish.',
+  img: 'bagel.jpg'
 }
 ];
 
