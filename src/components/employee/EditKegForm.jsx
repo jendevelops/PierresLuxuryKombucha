b723f9dd2c7e13f2e4bec27191b22e6d761b7d48
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-22 12:11:49 -0800

    Edit div structure of EditKegForm

--- src/components/employee/EditKegForm.jsx
+++ src/components/employee/EditKegForm.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function EditKegForm(props){
-  return(
+function EditKegForm(props) {
+  return (
     <div className='wrapper'>
-        <div className='splash'>
+      <div className='splash'>
         <h1>Edit Keg</h1>
         <form action="#/employee">
           <tr>
