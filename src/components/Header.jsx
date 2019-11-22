b66f22a7fa54de79e65aa33871a6e7e3ca8086f0
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-22 15:44:04 -0800

    Add conditional rendering dependent on login state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 
-function Header() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+function Header(props) {
+  let navbar = null;
+  if(props.loginState)
+  {
+    navbar = <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
+          <li className="nav-item">
+            <a className="nav-link" href="#/">Home<span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/kegs">Kegs<span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/kegs/new">Add New Keg<span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/employee/logout">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>;
+  }
+  else{
+    navbar = <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
             <a className="nav-link" href="#/">Home<span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
@@ -18,13 +44,11 @@ function Header() {
           <li className="nav-item">
             <a className="nav-link" href="#/employee/login">Login</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/employee">Employee View</a>
-          </li>
         </ul>
       </div>
-    </nav>
-  );
+    </nav>;
+  }
+  return navbar;
 }
 
 export default Header;
